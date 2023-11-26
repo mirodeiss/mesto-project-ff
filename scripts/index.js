267fd38a6bc7a94a60e5950adaecf2e15f8db847
Miro Deiss <mirodeiss@yahoo.com>
2023-11-26 12:47:36 +0300

    task-1 / create card function

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
-// @todo: Темплейт карточки
-
 // @todo: DOM узлы
-
+const cardsContainer = document.querySelector('.places__list')
 // @todo: Функция создания карточки
+function createCard(cardValue) {
+    // Темплейт карточки
+    const cardTemplate = document.querySelector('#card-template')
+    // клонируем содержимое этого темплейта 
+    const teplateContent = cardTemplate.content.cloneNode(true).querySelector('.places__item');
+    // содержимое темплейта
+    teplateContent.querySelector('.card__image').src = cardValue.link
+    teplateContent.querySelector('.card__image').alt = cardValue.name
+    teplateContent.querySelector('.card__title').textContent = cardValue.name
 
+    cardsContainer.append(teplateContent)
+    return teplateContent
+}
 // @todo: Функция удаления карточки
 
+function deleteCard(cardElement) {
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', () => {
+        cardElement.remove();
+    });
+}
+
 // @todo: Вывести карточки на страницу
+initialCards.forEach((item) => {
+    const cardElement = createCard(item)
+    deleteCard(cardElement)
+})
