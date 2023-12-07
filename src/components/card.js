58def4e49d16759c80f93942d83943620203c563
Miro Deiss <mirodeiss@yahoo.com>
2023-12-07 17:11:24 +0300

    like,submit, new card - functions

---
+++ src/components/card.js
@@ -0,0 +1,51 @@
+// функция like 
+export function likeCard(likeButton) {
+    likeButton.classList.toggle('card__like-button_is-active');
+}
+
+// Функция создания карточки
+
+export function createCard(cardValue, deleteCard, likeCard, openImageCard) {
+    // Темплейт карточки
+    const cardTemplate = document.querySelector('#card-template');
+
+    // Клонируем содержимое этого темплейта
+    const templateContent = cardTemplate.content.cloneNode(true).querySelector('.places__item');
+
+    // Содержимое темплейта
+    templateContent.querySelector('.card__image').src = cardValue.link;
+    templateContent.querySelector('.card__image').alt = cardValue.name;
+    templateContent.querySelector('.card__title').textContent = cardValue.name;
+
+    const deleteButton = templateContent.querySelector('.card__delete-button');
+    const likeButtons = templateContent.querySelectorAll('.card__like-button');
+    const imageItem = templateContent.querySelector('.card__image')
+
+    //  слушатель на кнопку удаления
+    deleteButton.addEventListener('click', () => {
+        deleteCard(templateContent);
+    })
+
+    // слушатель на кнопку лайка
+    likeButtons.forEach(likeButton => {
+        likeButton.addEventListener('click',()=>{
+            likeCard(likeButton);
+        });
+    });
+
+    // открытие фото 
+    imageItem.addEventListener('click', ()=>{
+        openImageCard(cardValue)
+    })
+
+    return templateContent;
+
+
+}
+
+
+// @todo: Функция удаления карточки
+
+export function deleteCard(cardElement) {
+    cardElement.remove();
+}
